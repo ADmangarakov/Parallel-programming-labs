4fd7fdec77aeeedf6c7837fe9bef38603335b45e
ADmangarakov <admangarakov@gmail.com>
2020-03-01 16:32:08 +0700

    Created repository, added lab1, mpi_test

---
+++ lab1/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+//#include "/usr/include/mpi/mpi.h"
+#include <mpi.h>
+#include <sys/time.h>
+#include <malloc.h>
+#include "Operations.h"
+
+int main(int argc, char *argv[]) {
+    struct timeval tv1, tv2;
+    int size, rank;
+    MPI_Init(&argc, &argv); // Инициализация MPI
+    MPI_Comm_size(MPI_COMM_WORLD, &size); // Получение числа процессов
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Получение номера процесса
+    printf("1Hello from process #%d of %d\n", rank, size);
+    int const PROCESS_NUMBER = size;
+    int const N = 12 / PROCESS_NUMBER;
+
+    gettimeofday(&tv1, NULL);
+    double *matr = NULL, *answ = NULL, *vector = NULL;
+    preset_solution(&matr, &answ, &vector, N, rank, size);
+    double const T = 1e-6;
+    do {
+        approximate(vector, matr, answ, N, T, rank, size);
+    } while (!is_answer_correct(vector, matr, answ, N, rank, size));
+
+    gettimeofday(&tv2, NULL);
+    double dt_sec = (tv2.tv_sec - tv1.tv_sec);
+    double dt_usec = (tv2.tv_usec - tv1.tv_usec);
+    double dt = dt_sec + 1e-6 * dt_usec;
+
+    printf("Hello from process #%d of %d\n", rank, size);
+
+    FILE *f = NULL;
+    char filename[50];
+    snprintf(filename, sizeof(char) * 50, "out_file%i.txt", rank);
+    f = fopen(filename, "w");
+
+    fprintf(f, "time diff %e \n", dt);
+    for (int i = 0; i < N; i++) {
+        fprintf(f, "%f\n", vector[i]);
+    }
+    fclose(f);
+    free(matr);
+    free(answ);
+    free(vector);
+    MPI_Finalize();
+    return 0;
+}
+
+
+
---
+++ mpi_test/cmake-build-debug/CMakeFiles/mpi_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpi_test.dir/main.c.o"
+  "mpi_test"
+  "mpi_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mpi_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
